6cb17ae31f8e9ca564be5057aec25da35476682e
Edwin <edwin@Edwins-MacBook-Air.local>
2016-08-16 09:33:52 +0100

    initial

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+exports.port = process.env.PORT || 3000;
+exports.mongodb = {
+  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://test:test@ds027165.mlab.com:27165/addressbook'
+};
+exports.companyName = 'Enginebox, Inc.';
+exports.projectName = 'Commevents';
+exports.systemEmail = 'enginebox2012@gmail.com';
+exports.cryptoKey = 'k3yb0ardc4t';
+exports.loginAttempts = {
+  forIp: 50,
+  forIpAndUser: 7,
+  logExpiration: '20m'
+};
+exports.requireAccountVerification = false;
+exports.smtp = {
+  from: {
+    name: process.env.SMTP_FROM_NAME || exports.projectName +' Website',
+    address: process.env.SMTP_FROM_ADDRESS || 'enginebox2012@gmail.com'
+  },
+  credentials: {
+    user: process.env.SMTP_USERNAME || 'enginebox2012@gmail.com',
+    password: process.env.SMTP_PASSWORD || 'Godsword1#',
+    host: process.env.SMTP_HOST || 'smtp.gmail.com',
+    ssl: true
+  }
+};
+exports.oauth = {
+  twitter: {
+    key: process.env.TWITTER_OAUTH_KEY || '',
+    secret: process.env.TWITTER_OAUTH_SECRET || ''
+  },
+  facebook: {
+    key: process.env.FACEBOOK_OAUTH_KEY || '',
+    secret: process.env.FACEBOOK_OAUTH_SECRET || ''
+  },
+  github: {
+    key: process.env.GITHUB_OAUTH_KEY || '',
+    secret: process.env.GITHUB_OAUTH_SECRET || ''
+  },
+  google: {
+    key: process.env.GOOGLE_OAUTH_KEY || '',
+    secret: process.env.GOOGLE_OAUTH_SECRET || ''
+  },
+  tumblr: {
+    key: process.env.TUMBLR_OAUTH_KEY || '',
+    secret: process.env.TUMBLR_OAUTH_SECRET || ''
+  }
+};
